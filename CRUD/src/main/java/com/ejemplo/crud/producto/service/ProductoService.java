d37e548335cef8fc52f96dd2b0297e6df75637a1
Enzo1s <enziitoomartin@gmail.com>
2021-07-05 14:48:34 -0300

    commons service

--- CRUD/src/main/java/com/ejemplo/crud/producto/service/ProductoService.java
+++ CRUD/src/main/java/com/ejemplo/crud/producto/service/ProductoService.java
@@ -1,8 +1,9 @@
 package com.ejemplo.crud.producto.service;
 
-import com.Commonsmicroservicios.services.EntityService;
+
+import com.Commonsmicroservicios.services.CommonService;
 import com.ejemplo.crud.producto.entity.Producto;
 
-public interface ProductoService extends EntityService<Producto>{
+public interface ProductoService extends CommonService<Producto>{
 
 }
--- CRUD/src/main/java/com/ejemplo/crud/producto/service/ProductoServiceImpl.java
+++ CRUD/src/main/java/com/ejemplo/crud/producto/service/ProductoServiceImpl.java
@@ -2,12 +2,12 @@ package com.ejemplo.crud.producto.service;
 
 import org.springframework.stereotype.Service;
 
-import com.Commonsmicroservicios.services.EntityServiceImpl;
+import com.Commonsmicroservicios.services.CommonServiceImpl;
 import com.ejemplo.crud.producto.entity.Producto;
 import com.ejemplo.crud.producto.repository.ProductoRepository;
 
 @Service
-public class ProductoServiceImpl extends EntityServiceImpl<Producto, ProductoRepository> implements ProductoService {
+public class ProductoServiceImpl extends CommonServiceImpl<Producto, ProductoRepository> implements ProductoService {
 
 
 }
